1c3e8d593b7be057e71f3d93d5f5206c713b2638
Danny Davila <danny@misaki.tech>
2024-01-31 05:26:03 +0900

    Next-Intl 1.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
-import Image from "next/image";
+import Image from 'next/image';
+import { useTranslations } from 'next-intl';
+import LanguageSelector from '@/components/languageSelector';
 
 export default function Home() {
+  const t = useTranslations('HomePage');
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -12,9 +16,10 @@ export default function Home() {
           height={38}
           priority
         />
+        <h1>{t('title')}</h1>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Get started by editing{' '}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>
@@ -50,6 +55,8 @@ export default function Home() {
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <LanguageSelector />
+
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
