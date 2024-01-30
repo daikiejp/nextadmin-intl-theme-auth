18351007ee3f71dcfc6c0beddc6ab98ae20b99e8
Danny Davila <danny@misaki.tech>
2024-01-31 06:59:43 +0900

    Next-Intl: Detect User's Language

--- services/locale.ts
+++ services/locale.ts
@@ -2,11 +2,17 @@
 
 import { cookies } from 'next/headers';
 import { Locale, defaultLocale } from '@/i18n/config';
+import { headers } from 'next/headers';
 
 const COOKIE_NAME = 'NEXT_LOCALE';
 
 export async function getUserLocale() {
-  return (await cookies()).get(COOKIE_NAME)?.value || defaultLocale;
+  const headersList = await headers();
+  const userAgent = headersList.get('accept-language');
+
+  return (
+    (await cookies()).get(COOKIE_NAME)?.value || userAgent || defaultLocale
+  );
 }
 
 export async function setUserLocale(locale: Locale) {
