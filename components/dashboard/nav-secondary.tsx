3b49f25dc3b11f4464fa2cc0d2a979e81e97e289
Danny Davila <danny@misaki.tech>
2025-05-11 14:13:31 +0900

    feat: 15.3.2-with-template

---
+++ components/dashboard/nav-secondary.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import * as React from "react";
+import { LucideIcon } from "lucide-react";
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+//import { warn } from "console";
+import { useTranslations } from "next-intl";
+
+export function NavSecondary({
+  items,
+  ...props
+}: {
+  items: {
+    title: string;
+    url: string;
+    icon: LucideIcon;
+  }[];
+} & React.ComponentPropsWithoutRef<typeof SidebarGroup>) {
+  const t = useTranslations("Dashboard.Sidebar");
+
+  return (
+    <SidebarGroup {...props}>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton asChild>
+                <a href={item.url}>
+                  <item.icon />
+                  <span>{t(item.title)}</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+}
